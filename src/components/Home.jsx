2ad28c5ba56e927c31ff5e09aec043001bc98793
OMSAH <huncyoms@gmail.com>
2024-11-12 05:27:43 -0500

    addex

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div>
+        <input type="text" />
+        <button>encrypt</button>
+        <button>decrypt</button>
+      
+    </div>
+  )
+}
